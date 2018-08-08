dbcd5fbfa8fb297fba28d82b3ff270b7b8d59fee
Anand R <anandr72@gmail.com>
2018-08-08 20:47:51 +0530

    Triggering a new build

--- Hello.java
+++ Hello.java
@@ -2,7 +2,6 @@ public class Hello {
 
     public static void main(String[] args) {
        System.out.println("Hello, World");
-         System.out.println("Triggering a new build all the way from Kolkata!!!");
     }
 
 }
